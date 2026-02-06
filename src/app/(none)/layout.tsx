3e02ad8dbf702f31851fa76e659164d246fcc420
stein <tairstenen@gmail.com>
2026-02-06 15:26:33 +0100

    Deleted leftover folders

--- src/app/(none)/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function EmptyLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  // Need to render using fragment otherwise it won't accept rendering children
-  return <>{children}</>;
-}

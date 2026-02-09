390e340470136240e24e897eed3d875f4baf3ebb
stein <tairstenen@gmail.com>
2026-02-10 00:48:28 +0100

    Changed to custom nextjs server handling

---
+++ server.ts
@@ -0,0 +1,27 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+import express from "express";
+import { Server } from "socket.io";
+
+// Reference:
+// https://github.com/dfjs/realtime-physical-spaces/blob/main/server.js
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
+const nextServer = next({ dev });
+const nextHandler = nextServer.getRequestHandler();
+
+nextServer.prepare().then(() => {
+  const expressServer = express();
+  const server = createServer(expressServer);
+  const io = new Server(server);
+  expressServer.get("/{*splat}", (req, res) => {
+    return nextHandler(req, res);
+  });
+  server.listen(port, () => {
+    console.log(
+      `Sever listening at http://localhost:${port} as ${dev ? `development` : process.env.NODE_ENV}`,
+    );
+  });
+});
